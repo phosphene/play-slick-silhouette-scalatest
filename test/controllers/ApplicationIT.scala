65c2a394c0332f6ba76d1b20aa66ea1b3144ad09
phosphene <ed@cronos.net>
2016-02-04 17:48:00 -0500

    passing tests

---
+++ test/controllers/ApplicationIT.scala
@@ -0,0 +1,35 @@
+package controllers
+
+import org.scalatest._
+
+
+import play.api.test._
+import play.api.test.Helpers._
+
+/**
+ * You can mock out a whole application including requests, plugins etc.
+ * For more information, consult the wiki.
+ */
+class ApplicationIT extends FunSpec with Matchers {
+
+  describe ("Application") {
+
+    it ("should send 404 on a bad request") {
+      running(FakeApplication()) {
+        val bad = route(FakeRequest(GET, "/boum")).get
+        status(bad) should be (NOT_FOUND)
+      }
+    }
+
+    it ("should render the index page") {
+      running(FakeApplication()) {
+        val result = route(FakeRequest(GET, "/")).get
+        status(result) should be (SEE_OTHER)
+        redirectLocation(result) should be (Some("/signIn"))
+        //status(home) should be (OK)
+        //contentType(home) should be (Some("text/html"))
+        //contentAsString(home) should include ("Sign In")
+      }
+    }
+  }
+}
